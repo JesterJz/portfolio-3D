f1492d170d1714b6f48a88bd5be7f1331c24e2d5
JesterJz <zimmer.jeyy@gmail.com>
2025-02-28 22:44:41 +0700

    making_route_dom make route and change text

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 
 import { arrow } from "../assets/icons";
 
+// eslint-disable-next-line react/prop-types
 const HomeInfo = ({ currentStage }) => {
   if (currentStage === 1)
     return (
       <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
         Hi, I'm
-        <span className='font-semibold mx-2 text-white'>Thanh Hoài</span>
+        <span className='font-semibold mx-2 text-white'>Thanh Hoai</span>
         👋
         <br />
         A Software Engineer from VietNam
